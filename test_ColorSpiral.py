fdcde0cbb584c071d38e4f349b465dab017b97e3
widdowquinn <leightonpritchard@mac.com>
2014-10-25 13:53:59 +0100

    Changed license text in headers.

--- test_ColorSpiral.py
+++ test_ColorSpiral.py
@@ -29,6 +29,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+
 """ Tests for general functionality of the ColorSpiral utility
 """
 
